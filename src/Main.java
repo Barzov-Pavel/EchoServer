a6501f2cacb9022c1875a52c7745519919d71ef3
Barzov-Pavel <barzov_pavel@mail.ru>
2020-01-21 11:04:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            while (true) {
+                new Echoer(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception " + e.getMessage());
+        }
+    }
+}
